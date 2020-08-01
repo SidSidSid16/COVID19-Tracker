08833f37c84b30537838149005e2874ef448ee5b
sidharth <sid@discover-tech.tk>
2020-08-01 21:26:56 +0100

    Added host ip argument

--- app.py
+++ app.py
@@ -498,6 +498,6 @@ if __name__ == "__main__":
     t = threading.Thread(target=start_initialise)
     t.daemon = True
     t.start()
-    app.run(debug = False, threaded=True)
+    app.run(host='0.0.0.0', debug = False, threaded=True)
 
     
