d3f82549aa7e8c8ecb84e9a35b5cb1f41283c2e5
Sidharth S <39242176+SidSidSid16@users.noreply.github.com>
2020-08-07 15:51:03 +0100

    Dataset update interval change
    - Changed the update interval from 5hrs to 3hrs

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def initialise():
     allCountriesTableCreate()
     print("Dataset Loaded!")
     is_initialising = False
-    threading.Timer(18000, initialise).start()
+    threading.Timer(10800, initialise).start()
 
 latestDatasetDate = None
 latestDatasetDateText = None
